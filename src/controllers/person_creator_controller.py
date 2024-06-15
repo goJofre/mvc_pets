97c11fcc0c865000b666df2d18ac1fa1c79841a6
goJofre <gabrielgoj3@gmail.com>
2024-06-14 22:33:25 -0300

    feat: Implementing error dir and adding it in controllers

--- src/controllers/person_creator_controller.py
+++ src/controllers/person_creator_controller.py
@@ -1,5 +1,6 @@
 import re
 from src.models.sqlite.interfaces.people_repository import PeopleRepositoryInterface
+from src.errors.error_types.http_bad_request import HttpBadRequestError
 from .interfaces.person_creator_controller import PersonCreatorControllerInterface
 
 class PersonCreatorController(PersonCreatorControllerInterface):
@@ -22,7 +23,7 @@ class PersonCreatorController(PersonCreatorControllerInterface):
         non_valid_caracters = re.compile(r'[^a-zA-Z]')
 
         if non_valid_caracters.search(first_name) or non_valid_caracters.search(last_name):
-            raise Exception("Nome da pessoa invalido!")
+            raise HttpBadRequestError("Nome da pessoa invalido!")
 
     def __insert_person_in_db(self, first_name: str, last_name: str, age: int, pet_id: int) -> None:
         self.__people_repository.insert_person(first_name, last_name, age, pet_id)
